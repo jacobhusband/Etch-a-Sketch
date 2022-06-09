7d6a8a43fcb8c1f3ec4837a0c9ccea0223e40aee
Jacob Husband <jakehusband2@gmail.com>
2022-06-08 18:28:49 -0700

    add color, erase, and reset features

--- script.js
+++ script.js
@@ -1,17 +1,73 @@
-for (let i = 0; i < 16; i++) {
-  let subContainer = document.createElement("div");
-  subContainer.className = "row";
-  for (let j = 0; j < 16; j++) {
-    let node = document.createElement("div");
-    node.className = "square";
-    node.addEventListener("mouseover", function (e) {
-      node.style.backgroundColor = "black";
-    });
-    subContainer.appendChild(node);
+const canvasSize = document.getElementById("myRange").value;
+const slider = document.getElementById("myRange");
+const stringSlider = `${slider.value} x ${slider.value}`;
+const h2Slider = document.createElement("h2");
+const sliderContainer = document.getElementById("slider-container");
+const colorButton = document.getElementById("color");
+const eraseButton = document.getElementById("erase");
+const resetButton = document.getElementById("reset");
+let color = "black";
+
+h2Slider.innerHTML = stringSlider;
+h2Slider.className = "flex-center";
+h2Slider.id = "h2-slider";
+sliderContainer.insertBefore(h2Slider, sliderContainer.children[0]);
+
+createCanvas(canvasSize);
+
+slider.addEventListener("input", function () {
+  h2Slider.innerHTML = `${this.value} x ${this.value}`;
+});
+
+slider.addEventListener("click", function () {
+  resetCanvas();
+});
+
+colorButton.addEventListener("click", function () {
+  color = "black";
+});
+
+eraseButton.addEventListener("click", function () {
+  color = "white";
+});
+
+resetButton.addEventListener("click", function () {
+  resetCanvas();
+});
+
+colorButton.click();
+
+function createCanvas(canvasSize) {
+  for (let i = 0; i < canvasSize; i++) {
+    let subContainer = document.createElement("div");
+    subContainer.className = "row";
+    for (let j = 0; j < canvasSize; j++) {
+      let node = document.createElement("div");
+      node.className = "square";
+      node.addEventListener("mouseover", function (e) {
+        node.style.backgroundColor = color;
+      });
+      subContainer.appendChild(node);
+    }
+    document
+      .getElementById("etch-a-sketch-container")
+      .appendChild(subContainer);
   }
-  document.getElementById("etch-a-sketch-container").appendChild(subContainer);
 }
 
-// const node = document.createElement("div");
-// const textnode = document.createTextNode("Yo");
-// document.getElementById("etch-a-sketch-container").appendChild(node);
+function resetCanvas() {
+  const divNodes = Array.from(document.getElementsByClassName("square"));
+  const etch = document.getElementById("etch-a-sketch-container");
+  const sliderValue = document.getElementById("myRange").value;
+  divNodes.forEach((node) => {
+    node.style.backgroundColor = "#fff";
+  });
+  removeAllChildNodes(etch);
+  createCanvas(sliderValue);
+}
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
